e76d429f4911836489082bf6f46406ecbf8de54e
Bogdan Nechita <talk.bogdan@gmail.com>
2014-10-10 15:57:09 +0300

    Princeps commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '7a6980059c74f8acc36d8c2678b2ecde95fdb12a9556cef780699eeab3bab81eafd7080c141d887c0b3b63e7c8fd46e8f3476d47c495db9c0eb181e77b81dacd'
