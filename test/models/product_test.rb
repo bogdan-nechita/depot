80912d81374c2498c75ec675b2561be638397474
Bogdan Nechita <talk.bogdan@gmail.com>
2014-11-26 20:42:11 +0200

    Add tests, validations, update fixtures.

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "product attributs must not be empty" do
+      product = Product.new
+      assert product.invalid?
+      assert product.errors[:title].any?
+      assert product.errors[:description].any?
+      assert product.errors[:price].any?
+	end
+
+	test "product price must be positive" do
+	  product = Product.new(title: 'New Product', description: 'Some description.', image_url: 'zzz.jpg')
+	  product.price = -2
+	  assert product.invalid?
+	  assert product.errors[:price].any?
+
+	end
+
+	test 'product title is at least 10 characters long' do
+		product = Product.new(title: 'Product')
+		assert product.invalid?
+		assert product.errors[:title].any?
+		assert product.errors[:title].first.include? 'too short'
+	
+	end
+
 end
---
+++ vendor/bundle/ruby/2.0.0/gems/debugger-1.6.8/ext/ruby_debug/siteconf20141119-5457-pl8jmw.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20141119-5457-1t6p958"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
